530a6529c44eeb3b5a4734787e7794f474f8a951
Derek Lee <DerekLeeCS@users.noreply.github.com>
2021-02-20 01:49:24 -0500

    testing alternative script

--- Project #2/connect.py
+++ Project #2/connect.py
@@ -8,16 +8,27 @@ LIM = 1
 if __debug__:
 	host = 'github.com'
 	ports = [ 21,22,23,25,80,443 ]
+#	host = 'synprint.com'
+#	ports = [ 21,22,23,25,80,110,139,443,445,3389 ]
 else:
 	host = input( "Please enter the target host: " )
 	ports = [ x for x in
 			input( "Please enter the list of ports to scan: " ).split(',') ]
 
 for port in ports:
+
 	try:
-		s = socket.create_connection( (host,port), timeout=LIM )
-		print( "Port " + "#" + str(port) + ": " +
-			s.recv(4096).decode().rstrip() )
+		s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		s.settimeout(LIM)
+		res = s.connect_ex( (host,port) )
+
+		if res == 0:
+			print( "Port " + "#" + str(port) + ": " +
+				s.recv(4096).decode().rstrip() )
+		else:
+			print( "Port " + "#" + str(port) + ": Connection failed." )
+
 		s.close()
-	except:
-		print( "Port " + "#" + str(port) + ": Connection failed." )
+
+	except socket.error:
+		print( "Port " + "#" + str(port) + ": ???" )
